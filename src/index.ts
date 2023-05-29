f5d9259968f073162646d3df94565a09d2190364
Ivan Nikolic <lesh@sysphere.org>
2023-05-29 14:46:42 +0300

    root project file

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+// data wide typing
+import * as types from './types'
+
+// utility functions
+import * as utils from './utils'
+
+// utility function wrappers
+import * as wrap from './wrap'
+
+// standard logger
+import * as logger from './logger'
+
+// generators
+import * as generator from './generator'
+
+// standard app initialization system
+import * as appCore from './appCore'
+
+export {
+    types,
+    utils,
+    wrap,
+    logger,
+    generator,
+    appCore
+}
