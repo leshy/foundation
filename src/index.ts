d185046a1d1976915c8c064b6accb0c3696e851e
Ivan Nikolic <lesh@sysphere.org>
2023-05-31 11:36:10 +0300

    configsystem included

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,15 @@ import * as generator from './generator'
 // standard app initialization system
 import * as appCore from './appCore'
 
+// standard app initialization system
+import * as configSystem from './configSystem'
+
 export {
     types,
     utils,
     wrap,
     logger,
     generator,
-    appCore
+    appCore,
+    configSystem
 }
